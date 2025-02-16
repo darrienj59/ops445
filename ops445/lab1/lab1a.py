fa080868b45053f376431ee6f33fafa9c8e79fca
darrien james <djames31@myseneca.ca>
2025-02-16 17:39:37 -0500

    Added lab1a

---
+++ ops445/lab1/lab1a.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hellow world" to the screen.'''
+
+# Any line that starts with a "#" os also known as a comment,
+print('Hello world')
