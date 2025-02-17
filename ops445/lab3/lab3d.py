d642306f613e32f0dec5103e2e954f0a4efbd0fe
darrien james <djames31@myseneca.ca>
2025-02-16 23:25:52 -0500

    lab3d added

---
+++ ops445/lab3/lab3d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Lab 3 Inv 2 function operate'''
+# Author ID: [140360199]
+
+import subprocess
+
+def free_space():
+    result = subprocess.run("df -h | grep '/$' | awk '{print $4}'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.stdout.decode('utf-8').strip()
+
+if __name__ == '__main__':
+    print(free_space())
