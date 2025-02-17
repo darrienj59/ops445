9b826f378d7616d9e2bc09d95bcd9fad47b5645f
darrien james <djames31@myseneca.ca>
2025-02-16 22:06:11 -0500

    Added lab2f

---
+++ ops445/lab2/lab2f.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Author: Darrien James
+#Author ID: 140360199
+#Date Create: 2025/02/16
+import sys
+timer = int(sys.argv[1])
+count = timer
+while count != 0:
+    print(count)
+    count = count - 1
+print("blast off!")
